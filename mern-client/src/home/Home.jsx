ea3f98025bc9a9820423da08a2d4d419bed1fbca
ishimwefrank123 <140093486+ishimwefrank123@users.noreply.github.com>
2025-07-08 12:50:09 +0200

    navbar component

--- mern-client/src/home/Home.jsx
+++ mern-client/src/home/Home.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <div>Home</div>
+      <div className=''>
+        <div className='h-screen'>Home
+        </div>
+        <div className='h-screen bg-red-600'></div>
+      </div>
     </div>
   )
 }
--- mern-client/src/routers/router.jsx
+++ mern-client/src/routers/router.jsx
@@ -26,7 +26,7 @@ const router = createBrowserRouter([
       },
       {
         path: '/blog',
-        element: <Blog/>
+        element: <Blog/>,
       }
     ]
   },
