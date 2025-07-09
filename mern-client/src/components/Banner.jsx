15358f38b312af67e6dd5c307180f2b5e25e9ed6
ishimwefrank123 <140093486+ishimwefrank123@users.noreply.github.com>
2025-07-09 14:16:54 +0200

    get promo section

--- mern-client/src/components/Banner.jsx
+++ mern-client/src/components/Banner.jsx
@@ -3,7 +3,7 @@ import BannerCard from '../home/BannerCard'
 
 const Banner = () => {
   return (
-    <div className='px-4 lg:px-24 bg-teal-100 flex items-center'>
+    <div className='px-4 lg:px-24 bg-blue-200 flex items-center'>
       <div className='flex w-full flex-col md:flex-row justify-between items-center gap-12 py-40'>
         {/* left side */}
         <div className='md:w-1/2 space-y-8 h-full'>
