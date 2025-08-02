5afc27a3316b9cbf9192a7706c8ec41168641ce3
ishimwefrank123 <140093486+ishimwefrank123@users.noreply.github.com>
2025-08-02 12:44:11 +0200

    show the user if available

--- mern-client/src/contects/AuthProvider.jsx
+++ mern-client/src/contects/AuthProvider.jsx
@@ -1,5 +1,5 @@
-import React, { createContext, useState } from 'react'
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import React, { createContext, useEffect, useState } from 'react'
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged } from "firebase/auth";
 import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext();
@@ -14,7 +14,18 @@ const AuthProvider = ({children}) => {
     return createUserWithEmailAndPassword(auth, email, password)
   }
 
+  useEffect(()=>{
+    const unsubscribe = onAuthStateChanged(auth, currentUser=> {
+      // console.log(currentUser);
+      setUser(currentUser)
+    });
+    return () => {
+      return unsubscribe();
+    }
+  },[])
+
   const authInfo = {
+    user,
     createUser
   }
 
